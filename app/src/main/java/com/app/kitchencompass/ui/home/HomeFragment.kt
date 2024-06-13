eea835cff9bb55731e776a5c1ffe77cce97de8ec
Levin Blummer <richard_levin.blummer@student.reutlingen-university.de>
2024-06-13 23:05:34 +0200

    Added Home random recipes

--- app/src/main/java/com/app/kitchencompass/ui/home/HomeFragment.kt
+++ app/src/main/java/com/app/kitchencompass/ui/home/HomeFragment.kt
@@ -5,11 +5,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.app.kitchencompass.Recipe
 import com.app.kitchencompass.databinding.FragmentHomeBinding
+import com.app.kitchencompass.ui.search.RecipeAdapter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.io.IOException
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
+    private val client = OkHttpClient()
+    private val baseURL: String = "http://23.236.54.96:9999/api/"
+    private lateinit var homeAdapter: RecipeAdapter
+    private lateinit var recyclerView: RecyclerView
 
     // This property is only valid between onCreateView and onDestroyView.
     private val binding get() = _binding!!
@@ -20,11 +37,42 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        return binding.root
+        val root: View = binding.root
+
+        // Initialize RecyclerView
+        recyclerView = binding.recyclerView // Assuming you have a RecyclerView in your layout with this ID
+        recyclerView.layoutManager = LinearLayoutManager(context)
+
+        // Load recipes
+        lifecycleScope.launch {
+            try {
+                val recipes: List<Recipe> = requestRecipes("random")
+                homeAdapter = RecipeAdapter(recipes)
+                recyclerView.adapter = homeAdapter
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+
+        return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    @Throws(IOException::class)
+    suspend fun requestRecipes(apiEndpoint: String): List<Recipe> {
+        val url = "$baseURL$apiEndpoint"
+        val request = Request.Builder().url(url).build()
+
+        return withContext(Dispatchers.IO) {
+            client.newCall(request).execute().use { response ->
+                val responseBody = response.body?.string() ?: throw IOException("Unexpected empty response body")
+                val recipeType = object : TypeToken<List<Recipe>>() {}.type
+                Gson().fromJson(responseBody, recipeType)
+            }
+        }
     }
 }
