ea064f8359d1521d85e64c13f5b1fdbce8064f1d
Levin Blummer <richard_levin.blummer@student.reutlingen-university.de>
2024-06-18 22:47:19 +0200

    some changes

--- app/src/main/java/com/app/kitchencompass/ui/search/SearchFragment.kt
+++ app/src/main/java/com/app/kitchencompass/ui/search/SearchFragment.kt
@@ -2,7 +2,6 @@ package com.app.kitchencompass.ui.search
 
 import android.content.Context
 import android.content.Intent
-import android.health.connect.datatypes.units.Length
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -21,7 +20,6 @@ import androidx.recyclerview.widget.RecyclerView
 import com.app.kitchencompass.DetailedActivity
 import com.app.kitchencompass.R
 import com.app.kitchencompass.Recipe
-import com.google.android.material.internal.ViewUtils.hideKeyboard
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import kotlinx.coroutines.Dispatchers
